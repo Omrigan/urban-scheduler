116969dec35656e810ead0bdc6ddace6c59badc1
Omrigan <omrigann@gmail.com>
2019-04-14 22:17:36 +0300

    Added rc

---
+++ global_tests/test_deployed.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from requests import get, post
+
+DOMAIN = "https://rc.urbanscheduler.ml"
+
+
+class TC(TestCase):
+    def test_params(self):
+        resp = get(DOMAIN+"/get_params")
+        self.assertEqual(resp.status_code, 200, resp.json())
+
+
+    def test_error(self):
+        sample = {
+            "ordered_events": [
+                {"type": "fixed_place"},
+            ]
+        }
+        resp = post(DOMAIN + '/predict', sample)
+        self.assertEqual(resp.status_code, 400, resp.json())
+
