ab6d877ff1474363b54a91b99910c69b6bd23a55
Oleg Vasilev <oleg.vasilev@intel.com>
2019-07-17 20:08:12 +0300

    Fix

--- front/src/components/Job.js
+++ front/src/components/Job.js
@@ -50,12 +50,12 @@ export class Job extends Component {
             this.setState({
                 options: options,
             });
-        }).then(() =>
+        }).then(() => {
              const problem = loadProblem();
              if(problem) {
 	    	this.setProblem(problem);
 	    }
-        );
+	});
 
 
         getCities(this.setState.bind(this));
